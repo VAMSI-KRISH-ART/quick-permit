b173c5050ed40665f0830532c7e67cde403fdf4c
VAMSI-KRISH-ART <vamsionteru6885@gmail.com>
2025-06-09 01:14:19 +0530

    Add files via upload

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,10 @@
+// Create a new folder 'utils' and add this file
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
+
+module.exports = generateToken;
