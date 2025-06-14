8f2ee3dd0a36e075eef43aad9ba0c48c251ae721
VAMSI-KRISH-ART <vamsionteru6885@gmail.com>
2025-06-15 02:15:21 +0530

    Delete backend directory

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getStudentProfile } = require('../controllers/userController');
-const { protect, studentOnly } = require('../middleware/authMiddleware');
-const router = express.Router();
-
-router.route('/student/profile').get(protect, studentOnly, getStudentProfile);
-// You can add a similar route for /warden/profile if needed
-
-module.exports = router;
